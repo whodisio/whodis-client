665b08ea508ed77028fa9f4ed554c641e9a2bb45
Ulad Kasach <uladkasach@gmail.com>
2020-12-16 19:48:34 -0600

    fix(deps): remove date-fns due to downstream bundle size

--- src/isTokenExpired.ts
+++ src/isTokenExpired.ts
@@ -1,8 +1,10 @@
-import fromUnixTime from 'date-fns/fromUnixTime';
-import isAfter from 'date-fns/isAfter';
 import { getUnauthedClaims } from 'simple-jwt-auth';
 
 import { WhodisAuthTokenClaims } from './WhodisAuthTokenClaims';
+
+// define basic date manipulation fns (dont import from a third party lib to decrease bundle size; this stuff is really basic too)
+const fromUnixTime = (seconds: number) => new Date(seconds * 1000);
+const isAfter = (referenceDate: Date, comparisonDate: Date) => referenceDate.getTime() > comparisonDate.getTime();
 
 export const isTokenExpired = ({ token }: { token: string }) => {
   const claims = getUnauthedClaims<WhodisAuthTokenClaims>({ token });
